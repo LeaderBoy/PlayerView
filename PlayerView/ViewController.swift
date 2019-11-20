dc9a44fc01ec91b4926a375036e3faa9ce5118f8
LeaderBoy <748603349@qq.com>
2019-11-20 14:18:41 +0800

    Initial Commit

---
+++ PlayerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerView
+//
+//  Created by 杨志远 on 2019/11/20.
+//  Copyright © 2019 BaQiWL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
