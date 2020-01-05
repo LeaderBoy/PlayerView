d5e620c17488cd489f22e01526cd9bcc05052e64
BaQiWL <748603349@qq.com>
2020-01-05 20:39:59 +0800

    1.remove cocoapods

--- Pods/Target Support Files/Pods-PlayerView/Pods-PlayerView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlayerView : NSObject
-@end
-@implementation PodsDummy_Pods_PlayerView
-@end
--- Pods/Target Support Files/Pods-PlayerViewUITests/Pods-PlayerViewUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlayerViewUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PlayerViewUITests
-@end
